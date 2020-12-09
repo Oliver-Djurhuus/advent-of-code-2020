4f6ce7e715ce96e723eb77aa08a6ab1000a63e94
Oliver Djurhuus <noquaz@gmail.com>
2020-12-09 08:14:56 +0100

    ok

--- 4/advent4.hs
+++ 4/advent4.hs
@@ -17,14 +17,14 @@ validField :: [String] -> Bool
 validField ["byr", x] = 1920 <= read x && read x <= 2002
 validField ["cid", _] = True
 validField ["ecl", x] = 1 == length (filter (\y -> y == x) ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"] )
-validField ["eyr", x] = length x == 4 && 2020 <= read x && read x <= 2030
+validField ["eyr", x] = 2020 <= read x && read x <= 2030
 validField ["hcl", x] = length x == 7 && head x == '#' && 0 == length (filter (==False) (map isHexDigit (tail x)))
 validField ["hgt", x] = (length x == 5 && 150 <= read (drop 0 (take 3 x)) && read (drop 0 (take 3 x)) <= 193 && "cm" == drop 3 (take 5 x)) || (length x == 4 && 59 <= read (drop 0 (take 2 x)) && read (drop 0 (take 2 x)) <= 76 && "in" == drop 2 (take 4 x))
-validField ["iyr", x] = length x == 4 && 2010 <= read x && read x <= 2020
+validField ["iyr", x] = 2010 <= read x && read x <= 2020
 validField ["pid", x] = length x == 9 && 9 == length (filter (\y -> isDigit y) x)
 
 isValid2 :: [[String]] -> Bool
-isValid2 passport = 0 == length (filter (/=True) (map validField passport))
+isValid2 passport = 0 == length (filter (==False) (map validField passport))
 
 
 -- Main
@@ -39,4 +39,3 @@ main = do
     print "Answer for part (b):"
     print $ length $ filter (\passport -> isValid1 passport && isValid2 passport) passports     
      
-
